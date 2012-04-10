ec1a3c40f298383349fca137e1a2c86d19df9cba
Detlev Ziereisen <zieredet@zhaw.ch>
2012-04-10 12:03:51 +0200

    chapter 8 Cosmetics

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
+    default_scope :order => 'title'
+
+	# validation stuff...
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.05}
 	validates :title, uniqueness: true
@@ -6,5 +10,4 @@ class Product < ActiveRecord::Base
 	with: %r{\.(gif|jpg|png)$}i,
 	message: 'he! is this a valid pic?'
 	}
-
 end
